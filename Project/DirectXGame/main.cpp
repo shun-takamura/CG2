f562c7a535be4d10aa9ff56c1366589e71a1cd6c
瞬 高村 <s.leonhart151110@gmail.com>
2026-06-06 14:10:04 +0900

    各関数の直前に処理内容を説明する日本語コメントを1行ずつ追加して。ロジックや既存コードは絶対に変えないこと。

--- Project/DirectXGame/main.cpp
+++ Project/DirectXGame/main.cpp
@@ -3,8 +3,10 @@
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	// Frameworkクラスの派生クラスGameをインスタンス化し、ユニークポインタで管理する
 	std::unique_ptr<Framework> game = std::make_unique<Game>();
 
+	// ゲームループを開始する
 	game->Run();
 
 	return 0;
